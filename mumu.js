55668dd4ab4e4240a8d3650812f27b4b2c4454e1
dawnbreak <1002395848@qq.com>
2024-04-02 00:37:33 +0800

    Update mumu.js

--- mumu.js
+++ mumu.js
@@ -1,32 +1,41 @@
-const url = $request.url;
-const isQuanX = typeof $task !== "undefined";
-const isLoon = typeof $loon != "undefined"
-const isSurge = typeof $httpClient != "undefined" && !isLoon
-let end_at = 1720086399
+/**
+ * @url: https://api-pro.mumu.163.com/api/v1/user/info/s
+ * @body: json
+    {
+      "code": 0,
+      "msg": "ok",
+      "data": {
+        "user_id": "aebglnr34uaaadua",
+        "nickname": "XXX****XXXX",
+        "member_expired_at": 0,
+        "member_status": 0,
+        "enabled_device_count": 1,
+        "current_device_status": 1,
+        "current_device": {
+          "device_id": "xxxxxxxxxxxx",
+          "alias": "XXX’s MacBook Pro",
+          "last_binded_at": 1711987410,
+          "trial_end_at": 1707494399,
+          "trial_status": 2
+        }
+      }
+    }
+*/
 
-console.log("我触发辣")
 
-if (!$response.body) $done({});
+if ($response.status !== 200) {$done({})};
+
 let obj = JSON.parse($response.body);
-
-
-if (url.includes("/api/v1/user/info")) {
-    obj.data.current_device.trial_end_at = end_at
-    obj.data.member_status = 1
-    obj.data.member_expired_at = end_at
-} else if (url.includes("/api/v1/device/current/trial")) {
-    obj.data.trial_end_at = end_at
-} else if (url.includes("/api/mac/pro/appcast/force")) {
-    obj.items = []
-} else if (url.includes("/api/mac/pro/appcast/remind")) {
-    obj.items = []
-} 
-
-
-if (isQuanX) {
-    $done({ body: JSON.stringify(obj) });
-} else if (isSurge) {
-    $done({ body: JSON.stringify(obj) });
-} else {
-    $done({ body: JSON.stringify(obj) });
+let end_at = 2516312087;
+if (obj.code === 0 && obj.data) {
+    let data = obj.data;
+    data.member_expired_at = end_at;
+    data.member_status = 1;
+    if (data.current_device) {
+        let device = data.current_device;
+        device.trial_end_at = end_at;
+        device.trial_status = 1;
+    }
 }
+
+$done({ body: JSON.stringify(obj)});
